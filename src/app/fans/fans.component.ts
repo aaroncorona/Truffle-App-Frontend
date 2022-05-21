45273fc403e77e2ce495d5ee6cf024f7ff091c52
= <amc1140@aol.com>
2022-05-20 21:52:11 -0700

    More Truffle photos!

--- src/app/fans/fans.component.ts
+++ src/app/fans/fans.component.ts
@@ -9,14 +9,12 @@ import {FansService} from "./fans.service";
 export class FansComponent {
   service: FansService;
   jsonGetResult: any;
-  jsonPushResult: any;
   names: any;
 
   constructor(service: FansService) {
     this.service = service
 
     // Get List of Fans (json to be parsed in HTML)
-    this.service.getFanList().subscribe(data => {console.log(data);})
     this.service.getFanList().subscribe(data => {this.jsonGetResult = data;})
 
     // Post user
